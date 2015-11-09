31e3736c81a43e30e1586ddc21aa5187a8c9d40b
Phillip Puckett <phillippuckett88@gmail.com>
2015-11-09 10:43:49 -0700

    Tool Bar, Mission Statement, Routes

--- filesJs/myApp.js
+++ filesJs/myApp.js
@@ -2,9 +2,14 @@ var app = angular.module('myApp', [])
 
 app.config(function ($stateProvider, $urlRouterProvider) {
 
-	$stateProvider
+    $stateProvider
         .state('home', {
             url: '/',
+            controller: 'mainCtrl'
+        })
+
+        .state('search', {
+            url: '/search',
             templateUrl: 'directiveFiles/dirTmpl.html',
             controller: 'mainCtrl'
         })
